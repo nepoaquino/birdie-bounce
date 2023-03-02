83bc1e4c24d63cb994daf1d391aebcecfa20d733
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-03-02 18:46:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let birdX = 50;
 let birdY = 200;
 let birdVelocity = 0;
-let gravity = 0.45;
-let gap = Math.floor(Math.random() * 3) * 20 + 130;
+const gravity = 0.45;
+let gap = getRandomGap();
 let pipeX = 400;
-let pipeY = Math.random() * 200 + 100;
+let pipeY = getRandomPipeY();
 let pipeWidth = 50;
 let pipeHeight = 600;
 let score = 0;
 let gameOver = false;
 let isGameStarted = false;
 
+function getRandomGap() {
+  return Math.floor(Math.random() * 3) * 20 + 130;
+}
+
+function getRandomPipeY() {
+  return Math.round(Math.random() * 200 + 100);
+}
 
 function drawPreview() {
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // draw background
+  ctx.fillStyle = "#d4e8de";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // draw bird
   ctx.fillStyle = "yellow";
@@ -30,16 +40,24 @@ function drawPreview() {
   ctx.fillRect(pipeX, pipeY + gap, pipeWidth, pipeHeight - pipeY - gap);
 
   // draw introduction text
-  ctx.textAlign ="center";
+  ctx.textAlign = "center";
   ctx.font = " bold 50px Avenir";
   ctx.fillText("Birdie Bounce", canvas.width / 2, canvas.height / 2 - 180);
   ctx.font = "18px Avenir";
   ctx.fillStyle = "gray";
-  ctx.fillText("created by: Nepo Aquino", canvas.width / 2, canvas.height / 2 - 150);
+  ctx.fillText(
+    "created by: Nepo Aquino",
+    canvas.width / 2,
+    canvas.height / 2 - 150
+  );
   ctx.fillStyle = "black";
   ctx.font = "24px Arial";
   ctx.fillText("Press Spacebar or", canvas.width / 2, canvas.height / 2 + 50);
-  ctx.fillText("Touch the screen to start", canvas.width / 2, canvas.height / 2 + 80);
+  ctx.fillText(
+    "Touch the screen to start",
+    canvas.width / 2,
+    canvas.height / 2 + 80
+  );
 }
 // draw initial preview
 drawPreview();
@@ -48,11 +66,15 @@ function draw() {
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // draw background
+  ctx.fillStyle = "#d4e8de";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
   // draw bird
   ctx.fillStyle = "yellow";
   ctx.fillRect(birdX, birdY, 30, 30);
 
-  // draw pipes 
+  // draw pipes
   ctx.fillStyle = "green";
   ctx.fillRect(pipeX, 0, pipeWidth, pipeY);
   ctx.fillRect(pipeX, pipeY + gap, pipeWidth, pipeHeight - pipeY - gap);
@@ -61,33 +83,36 @@ function draw() {
   ctx.fillStyle = "black";
   ctx.textAlign = "left";
   ctx.font = "25px Arial";
-  ctx.fillText(`Score: ${score}`, 10, 30); 
+  ctx.fillText(`Score: ${score}`, 10, 30);
 
   // update bird position
-  birdVelocity += gravity; 
+  birdVelocity += gravity;
   birdY += birdVelocity;
 
   // update pipe position
-   pipeX -= 3.5 + score * 0.02;
+  pipeX -= 3.5 + score * 0.02;
   if (pipeX + pipeWidth < 0) {
     pipeX = canvas.width;
     pipeY = Math.random() * 200 + 100;
-    gap = Math.floor(Math.random() * 3) * 20 + 130; // set a new gap value
+    gap = getRandomGap(); // set a new gap value
     score++;
   }
 
-  // check for collisions
+  //check for collisions
   if (
-    birdX < pipeX + pipeWidth &&
-    birdX + 30 > pipeX &&
-    (birdY < pipeY || birdY + 30 > pipeY + gap) ||
-    birdY + 30 > canvas.height
+    birdX + 25 > pipeX && // Bird hits right side of pipe
+    birdX < pipeX + pipeWidth && // Bird hits left side of pipe
+    (birdY + 5 < pipeY || birdY + 25 > pipeY + gap) // Bird hits top or bottom of pipe
   ) {
     gameOver = true;
-   }
+  }
+  if (birdY + 25 > canvas.height) {
+    // Bird hits bottom of screen
+    gameOver = true;
+  }
 
   // check for game over
-  if (birdY + 30 > canvas.height || gameOver) {
+  if (gameOver) {
     ctx.textAlign = "center";
     ctx.fillStyle = "red";
     ctx.font = "bold 50px Arial";
@@ -96,14 +121,16 @@ function draw() {
     ctx.font = "24px Arial";
     ctx.fillText(`Your Score: ${score}`, canvas.width / 2, canvas.height / 2);
     ctx.fillText("Press Spacebar or", canvas.width / 2, canvas.height / 2 + 50);
-  ctx.fillText("Touch the screen to restart", canvas.width / 2, canvas.height / 2 + 80);
+    ctx.fillText(
+      "Touch the screen to restart",
+      canvas.width / 2,
+      canvas.height / 2 + 80
+    );
     return;
-}
+  }
   // request next frame
   requestAnimationFrame(draw);
 }
-
-
 
 // listen for keypress
 document.addEventListener("keydown", function (event) {
@@ -117,14 +144,12 @@ document.addEventListener("keydown", function (event) {
     birdY = 200;
     birdVelocity = 0;
     pipeX = 400;
-    pipeY = Math.random() * 200 +100 ;
+    pipeY = getRandomPipeY();
     draw();
   } else if (event.key === " ") {
     birdVelocity = -8;
   }
 });
-
-
 
 // add touch event listeners to start the game and jump the bird
 document.addEventListener("touchstart", function (event) {
@@ -145,9 +170,7 @@ document.addEventListener("touchend", function (event) {
     birdY = 200;
     birdVelocity = 0;
     pipeX = 400;
-    pipeY = Math.random() * 200 + 100;
+    pipeY = getRandomPipeY();
     draw();
   }
 });
-
-
