07ac6415617984197f135624aee761cc73ea0530
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-03-05 18:37:12 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,31 +19,34 @@ self.addEventListener("install", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        // Return the cached response
-        return response;
-      }
-      // Not found in cache - fetch from network
-      return fetch(event.request)
-        .then(function (response) {
-          // Check if response is valid
-          if (!response || response.status !== 200 || response.type !== "basic") {
-            return response;
-          }
-          // Clone the response to cache
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(function (cache) {
-            cache.put(event.request, responseToCache);
-          });
-          return response;
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                // Return the cached response
+                return response;
+            }
+            // Not found in cache - fetch from network
+            return fetch(event.request)
+                .then(function (response) {
+                    // Check if response is valid
+                    if (
+                        !response ||
+                        response.status !== 200 ||
+                        response.type !== "basic"
+                    ) {
+                        return response;
+                    }
+                    // Clone the response to cache
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+                    return response;
+                })
+                .catch(function (err) {
+                    // Network request failed, return a fallback response
+                    return caches.match("/offline.html");
+                });
         })
-        .catch(function (err) {
-          // Network request failed, return a fallback response
-          return caches.match("/offline.html");
-        });
-    })
-  );
+    );
 });
-
