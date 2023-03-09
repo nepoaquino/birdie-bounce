3e9be10fdad1484d9d293129ae0f7a6c36c2d3b1
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-03-09 15:52:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,60 +1,34 @@
-// Define the cache name
-var cacheName = 'my-page-cache-v1';
-
-// Define the files to be cached
-var filesToCache = [
+const CACHE_NAME = 'birdie-bounce-cache-v1';
+const urlsToCache = [
   '/',
   'soundeffects/bump.wav',
   'soundeffects/fall.wav',
   'soundeffects/wingsFlap.wav',
   'sprite/bird.png',
   'sprite/birdfly.png',
+  'sprite/clouds.png',
   'index.html',
-  'index.js',
+  'index.js'
+
 ];
 
-// Install the service worker and cache the files
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Activate the service worker and remove old caches
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(name) {
-          return name !== cacheName;
-        }).map(function(name) {
-          return caches.delete(name);
-        })
-      );
-    })
-  );
-});
-
-// Intercept network requests and return cached files when offline
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      } else {
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
         return fetch(event.request);
-      }
-    })
+      })
   );
-});
-
-// Refresh the cache when the user presses the refresh button
-self.addEventListener('message', function(event) {
-  if (event.data.action === 'refresh') {
-    caches.open(cacheName).then(function(cache) {
-      cache.addAll(filesToCache);
-    });
-  }
 });
