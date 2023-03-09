c0c62d9ad307d71a5b0f5388fe3ab7a722429fe2
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-03-09 16:07:00 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
+// Set the name of the cache
 const CACHE_NAME = "birdie-bounce-cache-v1";
+
+// Set the URLs to cache
 const urlsToCache = [
   "/",
   "soundeffects/bump.wav",
@@ -11,14 +14,22 @@ const urlsToCache = [
   "index.js",
 ];
 
+// When the service worker is installed, open a cache and add the URLs to cache
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
+      try {
+        return cache.addAll(urlsToCache);
+      } catch (error) {
+        console.error("Failed to add URLs to cache:", error);
+      }
     })
   );
 });
 
+// When a fetch event occurs, respond with the cached version of the resource if it exists
+// If it doesn't exist, fetch the resource from the network
+// If the network request fails, serve the offline.html fallback page
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
@@ -33,6 +44,7 @@ self.addEventListener("fetch", function (event) {
   );
 });
 
+// When the service worker is activated, delete any old caches that have a different name than the current cache
 self.addEventListener("activate", function (event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
