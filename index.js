df7c8603ea8b2c2d01e12c5c51130ed27d306fa0
Nepo Aquino <nepo.comsci@gmail.com>
2023-11-03 06:10:07 +0800

    akjdajsnas

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 window.onload = function () {
   // Define the canvas and its context
   const canvas = document.getElementById("canvas");
@@ -26,37 +27,32 @@ window.onload = function () {
 
   // Define the pipes and their properties
   let pipeX = canvasWidth + 200;
-
   function getRandomPipeY() {
     return Math.round(Math.random() * 200 + 100);
   }
-
   let pipeY = getRandomPipeY();
   let pipeWidth = 50;
   let pipeHeight = 600;
-
   function getRandomGap() {
     return Math.round(Math.random() * 3) * 10 + 150;
   }
-
   let gap = getRandomGap();
 
   // Define the clouds and their properties
   const clouds = new Image();
   clouds.src = "sprite/clouds.png";
   let cloudsArr = [];
-
-  function createCloud() {
-    const cloudX = canvasWidth + Math.random() * canvasWidth;
-    const cloudY = Math.random() * 50;
-    const cloudVelX = -0.5;
-    const cloudWidths = [150, 200, 250];
-    const width = cloudWidths[Math.floor(Math.random() * cloudWidths.length)];
-    const cloud = { x: cloudX, y: cloudY, velX: cloudVelX, width: width };
-    cloudsArr.push(cloud);
-  }
-
-  createCloud();
+  let cloudX = Math.random() * canvasWidth;
+  let cloudY = Math.random() * 50;
+  let cloudVelX = -0.5;
+  const cloudWidths = [150, 200, 250]; // array of different widths for each cloud
+  let cloud = {
+    x: cloudX,
+    y: cloudY,
+    velX: cloudVelX,
+    width: cloudWidths[Math.floor(Math.random() * cloudWidths.length)],
+  };
+  cloudsArr.push(cloud);
 
   // Define game variables and functions
   const gravity = 0.4;
@@ -118,12 +114,11 @@ window.onload = function () {
 
       // Update pipe position
       pipeX -= speed + score * accelaration;
-
       // Check if a pipe has moved off the screen and reset it with a new gap and score
       if (pipeX + pipeWidth < 0) {
         pipeX = canvasWidth;
         pipeY = Math.random() * 200 + 100;
-        gap = getRandomGap();
+        gap = getRandomGap(); // Set a new gap value
         score++;
       }
 
@@ -131,7 +126,7 @@ window.onload = function () {
       if (
         birdX + birdWidth - 5 > pipeX && // Bird hits right side of pipe
         birdX < pipeX + pipeWidth && // Bird hits left side of pipe
-        (birdY + 10 < pipeY || birdY + birdHeight - 10 > pipeY + gap)
+        (birdY + 10 < pipeY || birdY + birdHeight - 10 > pipeY + gap) // Bird hits top or bottom of pipe
       ) {
         bump.play();
         gameOver = true;
@@ -151,7 +146,6 @@ window.onload = function () {
         ctx.fillStyle = "red";
         ctx.font = "bold 50px Arial";
         ctx.fillText("Game Over", canvasWidth / 2, canvasHeight / 2 - 100);
-
         // Display the restart button after 1 second
         setTimeout(function () {
           restartButton.style.display = "block";
@@ -170,26 +164,24 @@ window.onload = function () {
 
   // Draw bird
   function drawBird() {
+    // Update bird position
     ctx.save();
     ctx.translate(birdX + birdWidth / 2, birdY + birdHeight / 2);
     ctx.rotate(birdAngle);
     ctx.fillStyle = "yellow";
-    ctx.drawImage(
-      birdVelocity > 0 ? bird : birdfly,
-      -birdWidth / 2,
-      -birdHeight / 2,
-      birdWidth,
-      birdHeight
-    );
+    ctx.drawImage(bird, -birdWidth / 2, -birdHeight / 2, birdWidth, birdHeight);
     ctx.restore();
 
     birdVelocity += gravity;
     birdY += birdVelocity;
 
+    // Rotate bird based on velocity
     if (birdVelocity > 0) {
-      birdAngle = Math.min(Math.PI / 4, birdVelocity * 0.06);
+      bird.src = "sprite/bird.png";
+      birdAngle = Math.min(Math.PI / 4, birdVelocity * 0.06); // Going Down
     } else if (birdVelocity < 0) {
-      birdAngle = Math.max(-Math.PI, birdVelocity * 0.06);
+      bird.src = birdfly.src;
+      birdAngle = Math.max(-Math.PI, birdVelocity * 0.06); // Going Up
     }
   }
 
@@ -203,56 +195,72 @@ window.onload = function () {
     );
 
     if (pipeX + 80 <= pipeWidth * 2) {
-      pipeGradient.addColorStop(0, "#009c00");
-      pipeGradient.addColorStop(0.4, "#00bf00");
-      pipeGradient.addColorStop(0.6, "#00e600");
-      pipeGradient.addColorStop(1, "#00bf00");
+      // if first two pipes
+      pipeGradient.addColorStop(0, "#009c00"); // dark green
+      pipeGradient.addColorStop(0.4, "#00bf00"); // medium green
+      pipeGradient.addColorStop(0.6, "#00e600"); // light green
+      pipeGradient.addColorStop(1, "#00bf00"); // medium green
     } else {
-      pipeGradient.addColorStop(0, "#005c00");
-      pipeGradient.addColorStop(0.4, "#007f00");
-      pipeGradient.addColorStop(0.6, "#00a600");
-      pipeGradient.addColorStop(1, "#007f00");
+      // if other pipes
+      pipeGradient.addColorStop(0, "#005c00"); // dark green
+      pipeGradient.addColorStop(0.4, "#007f00"); // medium green
+      pipeGradient.addColorStop(0.6, "#00a600"); // light green
+      pipeGradient.addColorStop(1, "#007f00"); // medium green
     }
     ctx.fillStyle = pipeGradient;
 
+    // draw top pipe
     ctx.fillRect(pipeX - 5, pipeY - 30, pipeWidth + 10, 30);
     ctx.fillRect(pipeX, 0, pipeWidth, pipeY - 29.9);
 
-    const bottomPipeHeight = canvasHeight - pipeY - gap;
+    // draw bottom pipe
+    const bottomPipeHeight = canvasHeight - pipeY - gap; // calculate the height of the bottom pipe based on canvas height, pipe y, and gap
     ctx.fillRect(pipeX - 5, pipeY + gap, pipeWidth + 10, 30);
-    ctx.fillRect(pipeX, pipeY + gap + 30, pipeWidth, bottomPipeHeight);
+    ctx.fillRect(pipeX, pipeY + gap + 30, pipeWidth, bottomPipeHeight); // subtract 30 from bottomPipeHeight to account for the bottom pipe cap
   }
 
   function drawBackground() {
+    // Define the gradient sky colors
     const skyGradient = ctx.createLinearGradient(0, 0, 0, canvasHeight);
-    skyGradient.addColorStop(0, "#005588");
-    skyGradient.addColorStop(0.5, "#66ccff");
-    skyGradient.addColorStop(1, "#ffffff");
+    skyGradient.addColorStop(0, "#005588"); // Dark blue
+    skyGradient.addColorStop(0.5, "#66ccff"); // Light blue
+    skyGradient.addColorStop(1, "#ffffff"); // White
 
+    // Draw the sky gradient background
     ctx.fillStyle = skyGradient;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+    // calculate cloud speed
     let cloudspeed = speed + score * accelaration;
 
+    // draw clouds
     for (let i = 0; i < cloudsArr.length; i++) {
       let cloud = cloudsArr[i];
-      ctx.drawImage(clouds, cloud.x, cloud.y, cloud.width, 80);
+      ctx.drawImage(clouds, cloud.x, cloud.y, cloud.width, 80); // use the width property of the cloud
 
+      // calculate cloud velocity based on pipe speed
       const cloudVelX = -(cloudspeed / 2);
       cloud.x += cloudVelX;
 
       if (cloud.x + cloud.width < 0) {
-        cloudsArr.splice(i, 1);
-        i--;
-        continue;
+        // check if the cloud is outside the canvas
+        cloudsArr.splice(i, 1); // remove the cloud from the array
+        i--; // decrement the loop variable to compensate for the removed element
+        continue; // skip the rest of the loop iteration
       }
     }
 
+    // add a new cloud
     if (Math.random() < 0.007 && cloudsArr.length <= 4) {
-      createCloud();
+      let cloudX = canvasWidth + (Math.random() * canvasWidth) / 2;
+      let cloudY = (Math.random() * canvasHeight) / 2.5;
+      let width = cloudWidths[Math.floor(Math.random() * cloudWidths.length)];
+      let cloud = { x: cloudX, y: cloudY, width: width };
+      cloudsArr.push(cloud);
     }
   }
 
+  // Draw score
   function drawScore() {
     const gradient = ctx.createLinearGradient(0, 0, canvasWidth, 0);
     gradient.addColorStop(0, "#ff5722");
@@ -269,14 +277,17 @@ window.onload = function () {
   function drawIntroduction() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
+    // Define the gradient sky colors
     const skyGradient = ctx.createLinearGradient(0, 0, 0, canvasHeight);
-    skyGradient.addColorStop(0, "#005588");
-    skyGradient.addColorStop(0.5, "#66ccff");
-    skyGradient.addColorStop(1, "#ffffff");
+    skyGradient.addColorStop(0, "#005588"); // Dark blue
+    skyGradient.addColorStop(0.5, "#66ccff"); // Light blue
+    skyGradient.addColorStop(1, "#ffffff"); // White
 
+    // Draw the sky gradient background
     ctx.fillStyle = skyGradient;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+    // draw introduction text
     ctx.fillStyle = "green";
     ctx.textAlign = "center";
     ctx.font = " bold 50px Avenir";
@@ -289,18 +300,21 @@ window.onload = function () {
       canvasHeight / 2 - 150
     );
   }
-
+  // Game Preview
   drawIntroduction();
 
+  // Start the game loop
   function startGame() {
     isGameStarted = true;
     startButton.remove();
     requestAnimationFrame(gameLoop);
   }
 
+  // Add a start button click event listener
   startButton.addEventListener("click", startGame);
 
   restartButton.addEventListener("click", function () {
+    // Reset game variables to initial values
     birdY = 200;
     birdVelocity = 0;
     score = 0;
@@ -309,17 +323,22 @@ window.onload = function () {
     gap = getRandomGap();
     gameOver = false;
 
+    // Start the game again
     startGame();
 
+    // Hide restart button again
     restartButton.style.display = "none";
   });
 
+  // Check if user is on a mobile device
   const isMobileDevice =
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     );
 
   if (!isMobileDevice) {
+    // disable keydown and keyup listeners for mobile devices
+    // Add keydown event listener
     document.addEventListener("keydown", function (event) {
       if (event.key === " " && !controlling && isGameStarted === true) {
         playWingsFlap();
@@ -328,6 +347,7 @@ window.onload = function () {
       }
     });
 
+    // Add keyup event listener
     document.addEventListener("keyup", function (event) {
       if (event.key === " ") {
         controlling = false;
@@ -335,6 +355,7 @@ window.onload = function () {
     });
   }
 
+  // Add touchstart event listener to start the game and jump the bird
   document.addEventListener("touchstart", function (event) {
     if (isGameStarted === true) {
       playWingsFlap();
@@ -343,6 +364,7 @@ window.onload = function () {
     }
   });
 
+  // Function to make the bird flap
   function flapBird() {
     if (isGameStarted === true && !controlling) {
       playWingsFlap();
@@ -351,8 +373,10 @@ window.onload = function () {
     }
   }
 
+  // Add a mousedown event listener to the canvas for mouse click
   canvas.addEventListener("mousedown", flapBird);
 
+  // Add a mouseup event listener to simulate keyup behavior
   canvas.addEventListener("mouseup", function () {
     controlling = false;
   });
